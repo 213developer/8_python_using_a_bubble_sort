94dc9180beb3916e2c51039652e68f9b57f17837
“javascriptseo” <seofreelance2021@outlook.com>
2023-11-01 14:40:18 -0700

    project setup

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+HouseholdSize.py - This program uses a bubble sort to arrange household sizes
+                    in descending order and then prints the mean and median
+                    household size.
+Input:  Interactive.
+Output:  Mean and median household size.
+"""
+
+# Initialize variables.
+householdSizes = []  # Array used to store household sizes.
+numSizes = 0
+total = 0.0
+mean = 0.0
+median = 0.0
+
+# Input household size
+householdSizeString = input("Enter household size or 999 to quit: ")
+householdSize = int(householdSizeString)
+# This is the work done in the fillArray() function
+while (householdSize != 999):
+    # Place value in array.
+    householdSizes.append(householdSize)
+    # Calculate total of household sizes
+
+    numSizes += 1  # We have one more house
+    householdSizeString = input("Enter household size or 999 to quit: ")
+    householdSize = int(householdSizeString)
+
+# Find the mean
+
+# This is the work done in the sortArray() function
+
+# This is the work done in the displayArray() function
+
+# Find the median
